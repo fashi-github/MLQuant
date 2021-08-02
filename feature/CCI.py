b8687564b48567f630b0d3be63b82af371f28d7a
wanghai <hai.wang@mobilityasia.com.cn>
2021-08-02 11:17:58 +0800

    封装只获取CCI的方法

--- feature/CCI.py
+++ feature/CCI.py
@@ -9,24 +9,29 @@ def CCI(data, ndays):
     TP = (data['high'] + data['low'] + data['close']) / 3
     # CCI = pd.Series((TP - pd.rolling_mean(TP, ndays)) / (0.015 * pd.rolling_std(TP, ndays)),
     CCI = pd.Series((TP - TP.rolling(ndays).mean()) / (0.015 * TP.rolling(ndays).std()),
-    name='CCI')
+                    name='CCI')
     data = data.join(CCI)
     return data
 
 
-stock_data = st.get_csv_data('000002.XSHE', 'price')
+def nifty_cci(stock_code, ndays):
+    stock_data = st.get_csv_data(stock_code, 'price')
+    merged_data = CCI(stock_data, ndays)
+    return merged_data['CCI']
 
+
+XSHE000002_data = st.get_csv_data('000002.XSHE', 'price')
 
 # Compute the Commodity Channel Index(CCI) for NIFTY based on the 20-day Moving average
 n = 20
-NIFTY_CCI = CCI(stock_data, n)
+NIFTY_CCI = CCI(XSHE000002_data, n)
 CCI = NIFTY_CCI['CCI']
 
 # Plotting the Price Series chart and the Commodity Channel index below
 fig = plt.figure(figsize=(7, 5))
 ax = fig.add_subplot(2, 1, 1)
 ax.set_xticklabels([])
-plt.plot(stock_data['close'], lw=1)
+plt.plot(XSHE000002_data['close'], lw=1)
 plt.title('NSE Price Chart')
 plt.ylabel('Close Price')
 plt.grid(True)
---
+++ feature/__init__.py
@@ -0,0 +1,5 @@
+
+from CCI import nifty_cci
+
+cci = nifty_cci('000006.XSHE', 20)
+print('000002.XSHE cci: \n', cci)
