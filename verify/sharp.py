e3030272784fda8b3c64dffa7c71dd56547f618a
wanghai <hai.wang@mobilityasia.com.cn>
2021-08-10 17:54:40 +0800

    添加涨幅超过5%，作为买入策略的信号，并计算收益率及夏普比率

--- verify/sharp.py
+++ verify/sharp.py
@@ -13,9 +13,13 @@ pred_y = glob.get_value(glob.PRED_Y)
 # 因为在模型里面已经做了对齐，所以预测的索引就从零开始
 pred_index = 0
 profit_pct = pd.DataFrame()
+big_increase_pct = pd.DataFrame()
 # profit_pct = pd.DataFrame({'percent': 0}, index=[1000])
 percent_index = 0
 percent_array = list()
+big_increase_array = list()
+pred_cnt = 0
+big_increase_cnt = 0
 for i, close in test_y.items():
     if pred_index >= pred_y.size:
         break
@@ -28,26 +32,30 @@ for i, close in test_y.items():
             break
         # test_y[i + 1]就是close的下一天的收盘价格
         percent = (test_y[i + 1] - close) / close
+        if percent >= 0.05:
+            big_increase_array.append(percent)
+        pred_cnt = pred_cnt + 1
         # print("predict right !!", percent)
-        # print("predict df !!", pd.DataFrame(
-        #     {'percent': percent},
-        #     index=[percent_index]))
-        # profit_pct.append(pd.DataFrame(
-        #     {'percent': percent},
-        #     index=[percent_index]))
         # if percent > 0:
         percent_array.append(percent)
         # percent_index = percent_index + 1
     pred_index = pred_index + 1
 
 profit_pct['percent'] = percent_array
+print('predict correctly percent: ', float(pred_cnt / pred_index))
 print('profit mean: ', profit_pct.mean())
+big_increase_pct['percent'] = big_increase_array
+print('big increase profit mean: ', big_increase_pct.mean())
 
 avg_return = profit_pct.mean()
 sd_reutrn = profit_pct.std()
 # 计算夏普：每日收益率 * 252 = 每年收益率
 sharpe = avg_return / sd_reutrn
 print('Sharp: ', sharpe)
+
+avg_big_return = big_increase_pct.mean()
+std_big_return = big_increase_pct.std()
+print('Big Sharp: ', avg_big_return / std_big_return)
 # for index, row in test_y.iterrows():
 #     print(index, row[''])
 
